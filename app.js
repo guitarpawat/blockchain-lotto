70f5145b6fe11105cd1974a8008ac3aad896a6cc
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2020-02-20 18:26:41 +0700

    add support for test net

--- app.js
+++ app.js
@@ -6,25 +6,68 @@ const fs = require("fs");
 
 const Web3 = require('web3');
 const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:7545'));
-const contractSource = fs.readFileSync("./build/contracts/Random.json");
-const contractJSON = JSON.parse(contractSource)
-const contract = new web3.eth.Contract(contractJSON['abi'], contractJSON['networks']['5777']['address'])
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-if(!process.argv[2]) {
-    console.log('please specified address')
+if(process.argv.length !== 4) {
+    console.log('Usage:', process.argv[0], process.argv[1], '<network>', '<address index>')
+    console.log('network:       Ethereum network [local|ropsten]')
+    console.log('address index: Index in the provider address list (0 is contract deployer)')
+    console.log('');
     process.exit(1);
 }
 
+let netId;
+
+if(process.argv[2] === 'local') {
+    web3.setProvider(new web3.providers.HttpProvider('http://localhost:7545'));
+    netId = "5777";
+} else if(process.argv[2] === 'ropsten') {
+    const mnemonic = fs.readFileSync("mnemonic.txt").toString().trim();
+    const infuraApi = fs.readFileSync("infura_api.txt").toString().trim();
+    web3.setProvider(new HDWalletProvider(mnemonic, infuraApi));
+    netId = "3";
+} else {
+    console.log('invalid network:', process.argv[2]);
+    console.log('');
+    process.exit(1);
+}
+
+let defaultAccIndex = parseInt(process.argv[3])
+
+if(isNaN(defaultAccIndex) || defaultAccIndex < 0) {
+    console.log('please enter non-negative integer:', process.argv[3]);
+    console.log('');
+    process.exit(1);
+}
+
+let accounts
+let defaultAccount;
+let contract
+web3.eth.getAccounts().then(acc => {
+    accounts = acc;
+    console.log(acc);
+    if(defaultAccIndex < accounts.length) {
+        defaultAccount = accounts[defaultAccIndex];
+    } else {
+        console.log('account index out of bound, has length:', accounts.length);
+        console.log('');
+        process.exit(1);
+    }
+
+    const contractSource = fs.readFileSync("./build/contracts/Random.json");
+    const contractJSON = JSON.parse(contractSource);
+    contract = new web3.eth.Contract(contractJSON['abi'], contractJSON['networks'][netId]['address'])
+});
 
 app.get('/', (req, res) => {
     console.log('\n\nRandom contract call')
-    contract.methods.rand(50).send({ from: process.argv[2], gasLimit: "1000000" })
+    web3.eth.getBlockNumber().then(console.log);
+    contract.methods.rand(50).send({ from: defaultAccount, gasLimit: "5500000", gasPrice: "30000000000" })
     .then(result => {
         if(Array.isArray(result.events.Result)) {
             r = []
             result.events.Result.forEach(element => {
-                console.log(element.returnValues)
+                // console.log(element.returnValues)
                 r.push(
                     {
                         index: parseInt(element.returnValues.i), 
@@ -38,6 +81,7 @@ app.get('/', (req, res) => {
             });
             res.json(r)
         } else {
+            let element = result.events.Result;
             res.json(
                 {
                     index: parseInt(element.returnValues.i), 
@@ -49,6 +93,7 @@ app.get('/', (req, res) => {
                 }
             )
         }
+        console.log(result);
     })
 
 })
